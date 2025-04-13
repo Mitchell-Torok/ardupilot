d72ce0901030b6ae9bcdf998b8bbef20a05e348c
Mitchell-Torok <mitch.torok@gmail.com>
2025-04-13 20:26:07 +1000

    initial commit with failsafe test

--- ArduCopter/mode_turtle.cpp
+++ ArduCopter/mode_turtle.cpp
@@ -18,13 +18,6 @@ bool ModeTurtle::init(bool ignore_checks)
         return false;
     }
 
-    // do not enter the mode if sticks are not centered or throttle is not at zero
-    if (!is_zero(channel_pitch->norm_input_dz())
-        || !is_zero(channel_roll->norm_input_dz())
-        || !is_zero(channel_yaw->norm_input_dz())
-        || !is_zero(channel_throttle->norm_input_dz())) {
-        return false;
-    }
 
     // turn on notify leds
     AP_Notify::flags.esc_calibration = true;
@@ -43,7 +36,7 @@ void ModeTurtle::arm_motors()
 
     // reverse the motors
     hal.rcout->disable_channel_mask_updates();
-    change_motor_direction(true);
+    //change_motor_direction(true);
 
     // disable throttle and gps failsafe
     g.failsafe_throttle.set(FS_THR_DISABLED);
@@ -79,7 +72,7 @@ void ModeTurtle::disarm_motors()
     hal.util->set_soft_armed(false);
 
     // un-reverse the motors
-    change_motor_direction(false);
+    //change_motor_direction(false);
     hal.rcout->enable_channel_mask_updates();
 
     // re-enable failsafes
@@ -112,96 +105,99 @@ void ModeTurtle::change_motor_direction(bool reverse)
 
 void ModeTurtle::run()
 {
-    const float flip_power_factor = 1.0f - CRASH_FLIP_EXPO * 0.01f;
-    const bool norc = copter.failsafe.radio || !rc().has_ever_seen_rc_input();
-    const float stick_deflection_pitch = norc ? 0.0f : channel_pitch->norm_input_dz();
-    const float stick_deflection_roll = norc ? 0.0f : channel_roll->norm_input_dz();
-    const float stick_deflection_yaw = norc ? 0.0f : channel_yaw->norm_input_dz();
 
-    const float stick_deflection_pitch_abs = fabsf(stick_deflection_pitch);
-    const float stick_deflection_roll_abs = fabsf(stick_deflection_roll);
-    const float stick_deflection_yaw_abs = fabsf(stick_deflection_yaw);
 
-    const float stick_deflection_pitch_expo = flip_power_factor * stick_deflection_pitch_abs + power3(stick_deflection_pitch_abs) * (1 - flip_power_factor);
-    const float stick_deflection_roll_expo = flip_power_factor * stick_deflection_roll_abs + power3(stick_deflection_roll_abs) * (1 - flip_power_factor);
-    const float stick_deflection_yaw_expo = flip_power_factor * stick_deflection_yaw_abs + power3(stick_deflection_yaw_abs) * (1 - flip_power_factor);
 
-    float sign_pitch = stick_deflection_pitch < 0 ? -1 : 1;
-    float sign_roll = stick_deflection_roll < 0 ? 1 : -1;
-
-    float stick_deflection_length = sqrtf(sq(stick_deflection_pitch_abs) + sq(stick_deflection_roll_abs));
-    float stick_deflection_expo_length = sqrtf(sq(stick_deflection_pitch_expo) + sq(stick_deflection_roll_expo));
-
-    if (stick_deflection_yaw_abs > MAX(stick_deflection_pitch_abs, stick_deflection_roll_abs)) {
-        // If yaw is the dominant, disable pitch and roll
-        stick_deflection_length = stick_deflection_yaw_abs;
-        stick_deflection_expo_length = stick_deflection_yaw_expo;
-        sign_roll = 0;
-        sign_pitch = 0;
-    }
-
-    const float cos_phi = (stick_deflection_length > 0) ? (stick_deflection_pitch_abs + stick_deflection_roll_abs) / (sqrtf(2.0f) * stick_deflection_length) : 0;
-    const float cos_threshold = sqrtf(3.0f) / 2.0f; // cos(PI/6.0f)
-
-    if (cos_phi < cos_threshold) {
-        // Enforce either roll or pitch exclusively, if not on diagonal
-        if (stick_deflection_roll_abs > stick_deflection_pitch_abs) {
-            sign_pitch = 0;
-        } else {
-            sign_roll = 0;
-        }
-    }
-
-    // Apply a reasonable amount of stick deadband
-    const float crash_flip_stick_min_expo = flip_power_factor * CRASH_FLIP_STICK_MINF + power3(CRASH_FLIP_STICK_MINF) * (1 - flip_power_factor);
-    const float flip_stick_range = 1.0f - crash_flip_stick_min_expo;
-    const float flip_power = MAX(0.0f, stick_deflection_expo_length - crash_flip_stick_min_expo) / flip_stick_range;
-
-    // at this point we have a power value in the range 0..1
-
-    // normalise the roll and pitch input to match the motors
-    Vector2f input{sign_roll, sign_pitch};
-    motors_input = input.normalized() * 0.5;
-    // we bypass spin min and friends in the deadzone because we only want spin up when the sticks are moved
-    motors_output = !is_zero(flip_power) ? motors->thr_lin.thrust_to_actuator(flip_power) : 0.0f;
 }
-
+/*
 // actually write values to the motors
 void ModeTurtle::output_to_motors()
 {
-    // throttle needs to be raised
-    if (is_zero(channel_throttle->norm_input_dz())) {
-        const uint32_t now = AP_HAL::millis();
-        if (now - last_throttle_warning_output_ms > 5000) {
-            gcs().send_text(MAV_SEVERITY_WARNING, "Turtle: raise throttle to arm");
-            last_throttle_warning_output_ms = now;
-        }
 
+    if (failsafe.radio || failsafe.battery || failsafe.gcs || failsafe.ekf || failsafe.gps) {
+        disarm_motors();
+        return;
+    }
+
+    const int16_t rc_channel_5_value = rc().channel(4)->get_radio_in(); // RC channel 5 corresponds to index 4 (0-based index)
+
+    if (rc_channel_5_value < 1800) {
         disarm_motors();
         return;
     }
 
     arm_motors();
-
-    // check if motor are allowed to spin
     const bool allow_output = motors->armed() && motors->get_interlock();
+ 
 
-    for (uint8_t i = 0; i < AP_MOTORS_MAX_NUM_MOTORS; ++i) {
+    for (uint8_t i = 0; i < 2; ++i) {
         if (!motors->is_motor_enabled(i)) {
-            continue;
+            return;
         }
-
-        const Vector2f output{motors->get_roll_factor(i), motors->get_pitch_factor(i)};
-        // if output aligns with input then use this motor
-        if (!allow_output || (motors_input - output).length() > 0.5) {
+        if (!allow_output) {
             motors->rc_write(i, motors->get_pwm_output_min());
-            continue;
+            return;
         }
+        const int16_t rc_channel_0_value = rc().channel(i)->get_radio_in();
+        float normalized_input = (rc_channel_0_value - 988.0f) / (2012.0f - 988.0f);
+        normalized_input = constrain_float(normalized_input, 0.0f, 1.0f);
+        int16_t pwm = motors->get_pwm_output_min() + (motors->get_pwm_output_max() - motors->get_pwm_output_min()) *  fabsf(normalized_input);
 
-        int16_t pwm = motors->get_pwm_output_min() + (motors->get_pwm_output_max() - motors->get_pwm_output_min()) * motors_output;
 
         motors->rc_write(i, pwm);
     }
 }
+*/
+
+
+//Omnicopter FC
+
+
+// actually write values to the motors
+void ModeTurtle::output_to_motors()
+{
+
+    if (failsafe.radio || failsafe.battery || failsafe.gcs || failsafe.ekf || failsafe.gps) {
+        disarm_motors();
+        return;
+    }
+
+
+    const int16_t rc_channel_5_value = rc().channel(4)->get_radio_in(); // RC channel 5 corresponds to index 4 (0-based index)
+
+    if (rc_channel_5_value < 1800) {
+        disarm_motors();
+        return;
+    }
+
+    arm_motors();
+    const bool allow_output = motors->armed() && motors->get_interlock();
+    for (uint8_t i = 0; i < 2; ++i) {
+        if (!motors->is_motor_enabled(i)) {
+            return;
+        }
+        if (!allow_output) {
+            motors->rc_write(i, motors->get_pwm_output_min());
+            return;
+        }
+        const int16_t rc_channel_0_value = rc().channel(i)->get_radio_in();
+        float normalized_input = (rc_channel_0_value - 988.0f) / (2012.0f - 988.0f);
+        normalized_input = constrain_float(normalized_input, 0.0f, 1.0f);
+        int16_t pwm = motors->get_pwm_output_min() + (motors->get_pwm_output_max() - motors->get_pwm_output_min()) *  fabsf(normalized_input);
+
+
+        motors->rc_write(i, pwm);
+    }
+
+
+    //gcs().send_text(MAV_SEVERITY_INFO, "Normalized Input: %d", pwm);
+
+
+    int16_t netural = 1500;
+    motors->rc_write(2, netural);
+    motors->rc_write(3, netural);
+}
+    
+
 
 #endif
